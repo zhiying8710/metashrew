2dec39e9a3246d87ed82a8fdd5c2cb8bea1e6ef7
Binge <zhiying8710@hotmail.com>
2025-05-06 09:17:35 +0800

    debug

--- runtime/src/runtime.rs
+++ runtime/src/runtime.rs
@@ -468,7 +468,7 @@ where
     ) -> Result<bool> {
         let current_height = Self::parse_height_from_json(body)?;
         let latest = Self::check_latest_block_for_reorg(context.clone(), current_height)?;
-        
+        println!("[xxxx] Latest block height: {}, current height: {}", latest, current_height);
         Ok(latest != current_height)
     }
     
